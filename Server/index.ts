f88e74c797e4c3a3073eac03d67b7e597bfe3098
Panos Soile <panos.soilem@gmail.com>
2021-01-05 16:09:14 +0200

    Remove unused rooms

--- Server/index.ts
+++ Server/index.ts
@@ -2,12 +2,8 @@ import http from "http";
 import express from "express";
 import cors from "cors";
 
-import { Server, LobbyRoom } from "colyseus";
-import socialRoutes from "@colyseus/social/express";
-
-import { DemoRoom } from "./rooms/DemoRoom";
-import { FossilDeltaRoom } from "./rooms/FossilDeltaRoom";
-import { RoomWithoutState } from "./rooms/RoomWithoutState";
+import {Server} from "colyseus";
+import {ArrayTestRoom} from "./rooms/ArrayTestRoom";
 
 const PORT = Number(process.env.PORT || 2567);
 
@@ -25,17 +21,7 @@ const gameServer = new Server({
 });
 
 // Register DemoRoom as "demo"
-gameServer.define("demo", DemoRoom);
-gameServer.define("lobby", LobbyRoom);
-gameServer.define("fossildelta", FossilDeltaRoom);
-gameServer.define("no_state", RoomWithoutState);
-
-app.use("/", socialRoutes);
-
-app.get("/something", function (req, res) {
-  console.log("something!", process.pid);
-  res.send("Hey!");
-});
+gameServer.define("arrayTest", ArrayTestRoom);
 
 // Listen on specified PORT number
 gameServer.listen(PORT);
